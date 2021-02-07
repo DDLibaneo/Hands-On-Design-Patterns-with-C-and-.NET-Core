ce16f94bdc4798d0acc80733ce01146b3b78398f
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2021-02-07 13:55:06 -0300

    Add comentarios sobre a definição do Princípio de Liskov

--- Chapter2/LiskovPrinciple/Program.cs
+++ Chapter2/LiskovPrinciple/Program.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 
 namespace LiskovPrinciple
 {
+
+    // On the Liskov Principle:
+    // "Being able to substitute objects at runtime is the basis of the Liskov substitution principle.
+    // In OOP, if a class inherits from a base class or implements an interface, then it can be 
+    // referenced as an object of the base class or interface."
+
     class Program
     {
         static void Main(string[] args)
